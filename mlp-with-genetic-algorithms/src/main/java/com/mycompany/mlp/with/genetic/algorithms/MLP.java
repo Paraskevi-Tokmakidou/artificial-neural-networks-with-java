3e03dbd1a07b7ab0925f01c1b25e34f4e06669f5
Paraskevi Tokmakidou <paraskevitokmakidou@gmail.com>
2024-09-16 21:58:45 +0300

    Use for all the MLP, the class Double instead of double

--- mlp-with-genetic-algorithms/src/main/java/com/mycompany/mlp/with/genetic/algorithms/MLP.java
+++ mlp-with-genetic-algorithms/src/main/java/com/mycompany/mlp/with/genetic/algorithms/MLP.java
@@ -15,7 +15,7 @@ public class MLP {
     private final int _count;
     private final int _nodes;
     private final int _dimension;
-    private final double _learningRate;
+    private final Double _learningRate;
     private final int _maxEpoches;
     private final boolean _wantToDisplayTrainErrorInEachEpoch;
 
@@ -73,13 +73,13 @@ public class MLP {
         System.out.println("Unique classes: " + _uniqueOutputClasses);
     }
 
-    public double getOutput(ArrayList<Double> pattern) {
-        double sum = 0.0;
+    public Double getOutput(ArrayList<Double> pattern) {
+        Double sum = 0.0;
 
         for (int i = 0; i < this._nodes; i++) {
             int posbias = (this._dimension + 2) * i;
 
-            double arg = 0.0;
+            Double arg = 0.0;
             for (int j = 0; j < this._dimension; j++) {
                 arg += pattern.get(j) * this._weights.get(posbias) - (this._dimension + 1) + j
                         + this._weights.get(posbias);
@@ -91,7 +91,7 @@ public class MLP {
         return sum;
     }
 
-    public double getTrainError(Double wanted_ouput, Double output) { // E
+    public Double getTrainError(Double wanted_ouput, Double output) { // E
         return ((wanted_ouput - output) * (wanted_ouput - output));
     }
 
@@ -104,8 +104,8 @@ public class MLP {
 
         for (int i = 0; i < patterns.size(); i++) {
             // The desired output
-            double wanted_output = patterns.get(i).get(this._dimension);
-            double output = getOutput(patterns.get(i));
+            Double wanted_output = patterns.get(i).get(this._dimension);
+            Double output = getOutput(patterns.get(i));
             sum += (output - wanted_output) * (output - wanted_output);
         }
 
@@ -119,15 +119,15 @@ public class MLP {
         });
 
         for (int i = 0; i < this._nodes; i++) {
-            double arg = 0.0;
+            Double arg = 0.0;
 
             for (int j = 0; j < this._dimension; j++) {
                 int pos = (this._dimension + 2) * (i + 1) - (this._dimension + 1) + (j + 1);
                 arg += pattern.get(j) * this._weights.get(pos - 1);
             }
             arg += this._weights.get((this._dimension + 2) * (i + 1) - 1);
-            double s = MathematicalFunctions.sig(arg);
-            double s1 = MathematicalFunctions.sigder(arg);
+            Double s = MathematicalFunctions.sig(arg);
+            Double s1 = MathematicalFunctions.sigder(arg);
             patternDeriv.set((this._dimension + 2) * (i + 1) - (this._dimension + 1) - 1, s);
             patternDeriv.set((this._dimension + 2) * (i + 1) - 1,
                     this._weights.get((this._dimension + 2) * (i + 1) - (this._dimension + 1) - 1) * s1);
@@ -149,8 +149,8 @@ public class MLP {
 
         for (int i = 0; i < this._patterns.size(); i++) {
             ArrayList<Double> patternDeriv = getPatternDeriv(this._patterns.get(i));
-            double wanted_output = this._patterns.get(i).get(this._dimension);
-            double output = getOutput(this._patterns.get(i));
+            Double wanted_output = this._patterns.get(i).get(this._dimension);
+            Double output = getOutput(this._patterns.get(i));
 
             for (int j = 0; j < deriv.size(); j++) {
                 deriv.set(j, 2.0 * (output - wanted_output) * patternDeriv.get(j));
@@ -222,15 +222,15 @@ public class MLP {
 
         for (int i = 0; i < this._patterns.size(); i++) {
             // The desired output
-            double wanted_output = this._patterns.get(i).get(this._dimension);
-            double output = getOutput(this._patterns.get(i));
+            Double wanted_output = this._patterns.get(i).get(this._dimension);
+            Double output = getOutput(this._patterns.get(i));
             sum += (output - wanted_output) * (output - wanted_output);
         }
 
         return (sum / this._patterns.size());
     }
 
-    private void displayTrainError(int i, double trainError) {
+    private void displayTrainError(int i, Double trainError) {
         System.out.println("MLP Train Error: i[" + i + "] = " + trainError);
     }
 }
